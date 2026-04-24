bc39740ac274155507ce5d035c4fba4dcb91f73e
SerafimDogTraining <arthur@serafimdogtraining.com>
2026-04-23 22:42:56 -0500

    Fix HeroSection: correct divh typo from previous edit

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -19,7 +19,7 @@ export default function HeroSection() {
       <div className="absolute inset-0 bg-gradient-to-b from-black/55 via-black/35 to-black/25" />
 
       {/* Subtle grain/texture layer */}
-      <divh
+      <div
         className="absolute inset-0 opacity-[0.03]"
         style={{
           backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)'/%3E%3C/svg%3E")`,
